83fe8e2f89c18c26732f87edef55e2127dd9b6f3
ayocodingit <ayocodingit@gmail.com>
2021-09-08 22:20:26 +0700

    capture config fullpage

--- utils/capture.js
+++ utils/capture.js
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer'
 
 export default async (url) => {
   const filePath = `tmp/${Date.now()}${Math.random()}.png`
-  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] })
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.goto(url, { waitUntil: 'networkidle2' })
   await page.screenshot({ path: filePath, fullPage: true })

0e610d386dedff5cc8468bab2ec3d0d8eb1c4fad
ayocodingit <ayocodingit@gmail.com>
2021-09-09 16:21:36 +0700

    update random id

--- utils/capture.js
+++ utils/capture.js
@@ -13,7 +13,7 @@ const tagSubmit = process.env.TAG_SUBMIT
 
 export default async (url) => {
   const filePath = `tmp/${Date.now()}${Math.random()}.png`
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.setViewport({ width: 1024, height: 768, isLandscape: true })
   if (login === 'true') {

db07e9e0aa5cede7bc6641477be793b9561fc9bf
ayocodingit <ayocodingit@gmail.com>
2021-09-09 16:55:03 +0700

    remove login

--- utils/capture.js
+++ utils/capture.js
@@ -3,7 +3,6 @@ import puppeteer from 'puppeteer'
 import dotEnv from 'dotenv'
 dotEnv.config()
 
-const login = process.env.LOGIN
 const account = process.env.ACCOUNT
 const password = process.env.PASSWORD
 const urlLogin = process.env.URL_LOGIN
@@ -16,14 +15,14 @@ export default async (url) => {
   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.setViewport({ width: 1200, height: 768 })
-  await page.goto(urlLogin, { waitUntil: 'networkidle0' })
+  await page.goto(urlLogin, { waitUntil: 'networkidle2' })
   await page.type(tagUsername, account)
   await page.type(tagPassword, password)
   await Promise.all([
     page.click(tagSubmit),
-    page.waitForNavigation({ waitUntil: 'networkidle0' })
+    page.waitForNavigation({ waitUntil: 'networkidle2' })
   ])
-  await page.goto(url, { waitUntil: 'networkidle0' })
+  await page.goto(url, { waitUntil: 'networkidle2' })
   await page.screenshot({ path: filePath })
   await browser.close()
   return filePath

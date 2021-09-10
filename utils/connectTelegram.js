f15bc44f6105e8c57b7f4d17639f43144472b3d8
ayocodingit <ayocodingit@gmail.com>
2021-09-10 10:37:25 +0700

    update connect telegram

--- utils/connectTelegram.js
+++ utils/connectTelegram.js
@@ -15,9 +15,9 @@ const connectTelegram = async () => {
     connectionRetries: 5
   })
   await client.start({
-    phoneNumber: await input.text('Please enter your number: '),
-    password: await input.password('Please enter your password: '),
-    phoneCode: await input.text('Please enter the code you received: '),
+    phoneNumber: async () => await input.text('Please enter your number: '),
+    password: async () => await input.password('Please enter your password: '),
+    phoneCode: async () => await input.text('Please enter the code you received: '),
     onError: (err) => console.log(err)
   })
   console.log('You should now be connected.')

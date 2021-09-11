e0bd6f03e569dec7d248de0518c8084a5eb6dcbc
ayocodingit <ayocodingit@gmail.com>
2021-09-11 09:26:03 +0700

    update capture

--- utils/capture.js
+++ utils/capture.js
@@ -8,30 +8,29 @@ const password = Buffer.from(process.env.PASSWORD, 'base64').toString()
 
 const options = {
   github: {
-    tagUsername: '.js-login-field',
-    tagPassword: '.js-password-field',
-    tagSubmit: '.js-sign-in-button'
+    tagUsername: 'input[name=login]',
+    tagPassword: 'input[name=password]',
+    tagSubmit: 'input[type=submit]'
   },
   gitlab: {
     tagUsername: '#user_login',
     tagPassword: '#user_password',
-    tagSubmit: '.btn-confirm'
+    tagSubmit: 'input[type=submit]'
   }
 }
 
 export default async (url, git) => {
   const property = options[git]
   const filePath = `tmp/${Date.now()}${Math.random()}.png`
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-web-security'] })
   const page = await browser.newPage()
   await page.setViewport({ width: 1200, height: 768 })
-  await page.goto(url, { waitUntil: 'domcontentloaded' })
-  await page.waitForTimeout(5000)
+  await page.goto(url, { waitUntil: 'load' })
   if (await page.$(property.tagUsername) !== null) {
     await page.type(property.tagUsername, account)
     await page.type(property.tagPassword, password)
     await Promise.all([
-      page.click(property.tagSubmit, { delay: 2 }),
+      page.click(property.tagSubmit),
       page.waitForNavigation({ waitUntil: 'networkidle0' })
     ])
   }

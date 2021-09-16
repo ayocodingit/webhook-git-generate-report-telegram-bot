3268b7add09df5d156184f53cc0fba7ce07ee015
ayocodingit <ayocodingit@gmail.com>
2021-09-16 08:55:46 +0700

    add payload created_by

--- utils/payload.js
+++ utils/payload.js
@@ -6,14 +6,16 @@ const properties = {
     repository_url: ['body', 'pull_request', 'head', 'repo', 'html_url'],
     platform: ['git'],
     url: ['body', 'pull_request', 'html_url'],
-    body: ['body', 'pull_request', 'body']
+    body: ['body', 'pull_request', 'body'],
+    created_by: ['body', 'pull_request', 'user', 'login']
   },
   gitlab: {
     repository_name: ['body', 'repository', 'name'],
     repository_url: ['body', 'repository', 'homepage'],
     platform: ['git'],
     url: ['body', 'object_attributes', 'url'],
-    body: ['body', 'object_attributes', 'description']
+    body: ['body', 'object_attributes', 'description'],
+    created_by: ['body', 'user', 'username']
   }
 }
 
